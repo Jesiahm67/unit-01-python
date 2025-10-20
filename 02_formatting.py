8932d1004f0e0c2da0fffe8291fea44fbca5d9fa
Jesiah Minott <Jminott@brooklynsteamcenter.org>
2025-10-20 13:55:23 -0400

    02_formatting pt3

--- 02_formatting.py
+++ 02_formatting.py
@@ -39,7 +39,7 @@ Write a program that takes a person's name and age as input and prints it
 """
 name = input("Jesiah")#create inputs for name and age
 age = input("16")
-name = ("Jesiah")#create variables for the name and age
+name = ("Jesiah")#create variables for the name and age press enter for it to show up without difficulities
 age = ("16")
 print(f"My name is {name} and I am {age} years old.")#create print sentance
 
@@ -52,9 +52,9 @@ The result should be rounded to the nearest tenth (1 decimal place)
 num1 = float(input("Enter the first float: "))#create variables for float and input for both the first and second float
 num2 = float(input("Enter the second float: "))
 
-if num2 != 0:#create if and else statements for floats
+if num2 != 0:#create if statement to check if the second float is not equal to zero
     quotient = round(num1 / num2, 1) # Round to 1 decimal place
-    print("Quotient (rounded to 1 decimal):", quotient)
+    print("Quotient (rounded to 1 decimal):", quotient)#prints the quotient rounded to 1 decimal place
 
-else:
+else:#create else statement if the second number is equal to zero
     print("Cannot divide by zero.")
