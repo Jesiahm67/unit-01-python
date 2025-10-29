27f120ead4f9c4b5c50e45bf7d7ebabb5b4a58a0
Jesiah Minott <Jesiahm67@gmail.com>
2025-10-29 10:53:06 -0400

    commit future change

--- 09_debugging.py
+++ 09_debugging.py
@@ -1,3 +1,11 @@
+temperature = 75
+
+if temperature > 80:
+    print("It's hot")
+elif temperature > 50:
+    print("It's temperate")
+elif temperature < 0:
+    print("It's cold")
 "Exercise 1"
 
 text = "Hello, world, my name is"
